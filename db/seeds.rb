533a0b66ddeed53cb79edd18d29980f63c1d991a
tallkeith <tallestkeith@gmail.com>
2015-04-02 11:50:32 -0400

    Added a people landing page

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+require 'faker'
+ 
+40.times do 
+Person.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, birthdate: Faker::Date.between(80.years.ago, 18.years.ago))
+end
